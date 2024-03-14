17a62ad50ba420eb89d9f9ac54603eb068857651
Iain Bullock <59146297+iainbullock@users.noreply.github.com>
2024-03-14 17:18:58 +0000

    Update config.sh
    Can't use IP address for PROXY_HOST

--- tesla_http_proxy/app/config.sh
+++ tesla_http_proxy/app/config.sh
@@ -3,7 +3,7 @@
 CLIENT_ID=client_id
 CLIENT_SECRET=client_secret
 DOMAIN=tesla.example.com # Public FQDN 
-PROXY_HOST=192.168.0.3 # Local hostname or IP of this docker host
+PROXY_HOST=docker_host # Local hostname (not IP) of this docker host
 REGION='Europe, Middle East, Africa' # Change to match your region
 # Change this to OPTIONS_COMPLETE=1 when ready to run
 OPTIONS_COMPLETE=0
