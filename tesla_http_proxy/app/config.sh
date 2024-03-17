05b0655f19ec92eeca39d1c316f7e8ef9e7d2da8
Iain Bullock <59146297+iainbullock@users.noreply.github.com>
2024-03-17 17:00:12 +0000

    Update config.sh
    Allow port to be specified for the proxy

--- tesla_http_proxy/app/config.sh
+++ tesla_http_proxy/app/config.sh
@@ -4,6 +4,7 @@ CLIENT_ID=client_id
 CLIENT_SECRET=client_secret
 DOMAIN=tesla.example.com # Public FQDN 
 PROXY_HOST=docker_host # Local hostname (not IP) of this docker host
+PROXY_PORT=4430 # Port for the proxy
 REGION='Europe, Middle East, Africa' # Change to match your region
 # Change this to OPTIONS_COMPLETE=1 when ready to run
 OPTIONS_COMPLETE=0
