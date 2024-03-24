4f5b8b6bdae3eb3cbd38af0feb4b99908b7c2344
Iain Bullock <59146297+iainbullock@users.noreply.github.com>
2024-03-24 14:45:56 +0000

    Create open_boot.sh

---
+++ tesla_http_proxy/app/curl/open_boot.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#TESLA_AUTH_TOKEN=XXXXXX
+#VIN=xxxxxxxxxxxxxxxxx
+
+curl --cacert ../cert.pem \
+    --header 'Content-Type: application/json' \
+    --header "Authorization: Bearer $TESLA_AUTH_TOKEN" \
+    --data '{"which_trunk": "rear"}' \
+    "https://$PROXY_HOST:4430/api/1/vehicles/$VIN/command/actuate_trunk"
