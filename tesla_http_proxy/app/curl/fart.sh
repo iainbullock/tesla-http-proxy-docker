83415ed5cfeb6e486bd5f491c58c22c0b168c3fe
Iain Bullock <59146297+iainbullock@users.noreply.github.com>
2024-03-24 14:48:42 +0000

    Update fart.sh

--- tesla_http_proxy/app/curl/fart.sh
+++ tesla_http_proxy/app/curl/fart.sh
@@ -6,4 +6,4 @@ curl --cacert ../cert.pem \
     --header 'Content-Type: application/json' \
     --header "Authorization: Bearer $TESLA_AUTH_TOKEN" \
     --data '{"sound": "1"}' \
-    "https://macmini.home:4430/api/1/vehicles/$VIN/command/remote_boombox"
+    "https://$PROXY_HOST:4430/api/1/vehicles/$VIN/command/remote_boombox"
